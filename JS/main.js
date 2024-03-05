353c3db25e93bc9f35091ad2915c3556d53260b6
Kayceekeys <102333973+Kayceekeys@users.noreply.github.com>
2024-03-05 15:35:26 +0100

    maxx

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,17 @@
         clearInterval(countdownInterval);
       }
     }, 1000);                                              
+
+
+
+
+const menuContainer = document.getElementById("nav__menu");
+
+const openMenu = () => {
+
+    if(menuContainer.classList.contains("nav_open")){
+        menuContainer.classList.remove("nav_open");
+    }else{
+        menuContainer.classList.add("nav_open");
+    }
+}
